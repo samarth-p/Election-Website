5f48717d42bff4894fbf7b09159e3507430b829a
Shahid Nehal A <41267809+neehu613@users.noreply.github.com>
2019-03-30 21:29:59 +0530

    Added templates, static files, urls.py, views.py

--- electionnaire/urls.py
+++ electionnaire/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home),
+    path('login/', views.login),
+    path('login/confirm/', views.confirm),
+    path('loggedIn/', views.loggedIn),
+    path('loggedIn/success/', views.success),
+    path('loggedIn/vote/', views.vote),
+    path("login/eci/", views.eci_login),
+    path('loggedIn_eci/', views.loggedIn_eci),
 ]
--- electionnaire/views.py
+++ electionnaire/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, render_to_response
+from electionnaire.forms import CaptchaTestForm
 # Create your views here.
+def home(request):
+	return render(request, 'electionnaire/home.html')
 
+def login(request):
+    if request.POST:
+        form = CaptchaTestForm(request.POST)
+        if form.is_valid():
+            human = True
+    else:
+        form = CaptchaTestForm()
 
-def index(request):
-    return HttpResponse("Election boi")
+    return render_to_response('electionnaire/login.html',locals())
+
+def confirm(request):
+	return render(request, 'electionnaire/otp.html')
+
+def loggedIn(request):
+	return render(request, 'electionnaire/loggedIn.html')
+
+def success(request):
+	return render(request, 'electionnaire/success.html')
+
+def vote(request):
+	return render(request, 'electionnaire/vote.html')
+
+def eci_login(request):
+	return render(request, 'electionnaire/eci_login.html')
+
+def loggedIn_eci(request):
+	return render(request, 'electionnaire/loggedIn_eci.html')
