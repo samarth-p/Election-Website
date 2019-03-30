8d4e3c3a2fdf7d2b6f7fecef07fca39db5b1e6d3
Samarth <samarthp1@gmail.com>
2019-03-30 22:54:33 +0530

    changed view

--- electionnaire/views.py
+++ electionnaire/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, render_to_response
 from electionnaire.forms import CaptchaTestForm
 # Create your views here.
+
+
 def home(request):
 	return render(request, 'electionnaire/home.html')
+
+
 
 def login(request):
     if request.POST:
@@ -14,20 +18,32 @@ def login(request):
 
     return render_to_response('electionnaire/login.html',locals())
 
+
+
 def confirm(request):
 	return render(request, 'electionnaire/otp.html')
+
+
 
 def loggedIn(request):
 	return render(request, 'electionnaire/loggedIn.html')
 
+
+
 def success(request):
 	return render(request, 'electionnaire/success.html')
+
+
 
 def vote(request):
 	return render(request, 'electionnaire/vote.html')
 
+
+
 def eci_login(request):
 	return render(request, 'electionnaire/eci_login.html')
+
+
 
 def loggedIn_eci(request):
 	return render(request, 'electionnaire/loggedIn_eci.html')
