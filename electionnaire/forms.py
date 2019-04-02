ddd8e34b0de7681165e09930bbf3831ebcb317de
Shahid Nehal A <41267809+neehu613@users.noreply.github.com>
2019-04-02 15:59:00 +0530

    Added Electionnaire app

--- electionnaire/forms.py
+++ electionnaire/forms.py
@@ -3,3 +3,7 @@ from captcha.fields import CaptchaField
 
 class CaptchaTestForm(forms.Form):
     captcha = CaptchaField()
+
+class PostForm(forms.Form):
+    image = forms.ImageField()
+    caption = forms.CharField()
